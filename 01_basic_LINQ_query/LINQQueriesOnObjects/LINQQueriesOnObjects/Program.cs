08f8205b3ddadc3bc6d5fae3b6711dafbc4188ea
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-08 17:13:51 +0100

    Intro

---
+++ 01_basic_LINQ_query/LINQQueriesOnObjects/LINQQueriesOnObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQQueriesOnObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
