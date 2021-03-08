c3b3a4b5d1860d8f8330491b0d8f4d4b93714912
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-08 17:04:45 +0100

    LINQ intro examples

---
+++ 01_basic_LINQ_querty/LINQExamples/LINQExamples/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExamples
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = { "hello", "world", "bye", "hi" };
+            int[] numbers = { 1, 5, 8, 3 };
+
+            var wordsWithE = from word in words
+                             // same as: where word.Contains('e') && (word.Length < 5)
+                             where word.Contains('e')
+                             where word.Length < 5
+                             select word;
+
+            foreach (var word in wordsWithE)
+                Console.WriteLine(word);
+
+            var getTheNumbers = from n in numbers
+                                where n > 1
+                                orderby n descending
+                                select n;
+
+            foreach (var n in getTheNumbers)
+                Console.WriteLine(n);
+        }
+    }
+}
