8141f5790ce4d21208f52dbfe8b38475a4b48d5b
Anna Ossowska <ossowska.anna1@gmail.com>
2021-03-11 12:28:16 +0100

    Select and Where in method syntax

---
+++ 04_LINQ_method_syntax_intro/SelectvsWhere/SelectvsWhere/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SelectvsWhere
+{
+    public class Person
+    {
+        public int Height { get; set; }
+    }
+
+    class Program
+    {
+        public static int[] StringToIntArray(string numbers)
+        {
+            var arrayFromString = numbers.Split(' ')
+                                         .Select(n => int.Parse(numbers))
+                                         .ToArray();
+            return arrayFromString;
+        }
+
+        static void Main()
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person() { Height = 152 },
+                new Person() { Height = 180 },
+                new Person() { Height = 190 },
+                new Person() { Height = 175 },
+                new Person() { Height = 153 },
+            };
+
+            // Select returns a new collection from another collection
+            var shortPeople = people.Where(p => p.Height < 155)
+                                    .Select(p => p.Height);
+
+            foreach (var p in shortPeople)
+                Console.WriteLine(p);
+        }
+    }
+}
